953c9f7a2a0653d253fe37ddebd3badecd3ca14b
huangyunt <973113137@qq.com>
2025-05-03 23:27:47 +0800

    feat: 顶栏,底栏样式

--- src/pages/Components/BottomBar/BottomBar.tsx
+++ src/pages/Components/BottomBar/BottomBar.tsx
@@ -1,13 +1,17 @@
+
 import React from "react";
 import { AtTabBar } from "taro-ui";
+import { Tabs } from "@tarojs/components";
 
 export const BottomBar: React.FC = () => {
   return (
     <AtTabBar
       fixed
+      current={1}
+      onClick={() => { }}
+      color="rgb(80, 80, 80)"
+      backgroundColor="rgb(245, 245, 245)"
       tabList={[{ title: "书架", iconType: "folder" }]}
-      onClick={() => {}}
-      current={0}
     />
   );
 };
--- src/pages/Components/TopTab/TopTab.tsx
+++ src/pages/Components/TopTab/TopTab.tsx
@@ -1,29 +1,10 @@
 import React, { useState } from "react";
-import { View, Text, Image } from "@tarojs/components";
-import { AtTabs, AtTabsPane } from "taro-ui";
+import { View, Text } from "@tarojs/components";
+import "./TopTab.scss"
 
-export const TopTab: React.FC = () => {
-  const tabList = [
-    { title: "全部" },
-    // { title: "标签页2" },
-    // { title: "标签页3" },
-  ];
-
-  const [current, setCurrent] = useState(1);
-  return (
-    // <AtTabs
-    //   current={current}
-    //   tabList={tabList}
-    //   onClick={(val) => setCurrent(val)}
-    // >
-    //   <AtTabsPane current={current} index={0}>
-    //     {/* <View style="padding: 100px 50px;background-color: #FAFBFC;text-align: center;">
-    //       标签页一的内容
-    //     </View> */}
-    //   </AtTabsPane>
-    // </AtTabs>
-    <View className="book-item">
-      <Text>全部</Text>
-    </View>
-  );
-};
+export const TopTab: React.FC = () => (
+  <View className="haisha-top-bar">
+    <Text className="text">全部</Text>
+    <View className="rect"></View>
+  </View>
+)
