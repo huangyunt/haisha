b70657410b0700c763c19c51cd3d1cc710013c27
huangyunt <973113137@qq.com>
2025-06-05 14:03:50 +0800

    修复音频问题

--- src/pages/BookDetail/Components/BookPreview/constants/audioList.ts
+++ src/pages/BookDetail/Components/BookPreview/constants/audioList.ts
@@ -2304,14 +2304,14 @@ const audioList3 = {
     // 没给音频
     91: [
         {
-            offset: [954, 789],
+            offset: [933, 527],
             url: 'https://636c-cloud1-6geu18jg425a604e-1360744728.tcb.qcloud.la/%E7%AC%AC%E4%B8%89%E6%9C%AC%E7%BB%98%E6%9C%AC%E9%83%A8%E5%88%86%E9%9F%B3%E9%A2%91%EF%BC%88%E6%8C%89%E9%A6%96%E5%AD%97%E6%AF%8D%E9%A1%BA%E5%BA%8F%EF%BC%89/i%20draw%20a%20bunny.mp3'
         },
     ],
     // 没给音频
     94: [
         {
-            offset: [961, 762],
+            offset: [933, 525],
             url: 'https://636c-cloud1-6geu18jg425a604e-1360744728.tcb.qcloud.la/%E7%AC%AC%E4%B8%89%E6%9C%AC%E7%BB%98%E6%9C%AC%E9%83%A8%E5%88%86%E9%9F%B3%E9%A2%91%EF%BC%88%E6%8C%89%E9%A6%96%E5%AD%97%E6%AF%8D%E9%A1%BA%E5%BA%8F%EF%BC%89/i%20dream.mp3'
         },
     ],
